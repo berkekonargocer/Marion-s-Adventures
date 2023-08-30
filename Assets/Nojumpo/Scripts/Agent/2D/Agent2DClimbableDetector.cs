a1362f700077019689c706aadafd9d0a4fc02bde
Berke Konargöçer <berke.konargocer@gmail.com>
2023-08-31 00:49:48 +0300

    Agent2DClimbableDetector.cs Created and Impelemented

---
+++ Assets/Nojumpo/Scripts/Agent/2D/Agent2DClimbableDetector.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class Agent2DClimbableDetector : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+        [SerializeField] LayerMask climbableLayerMask;
+
+        [field: SerializeField] public bool CanClimb { get; private set; }
+
+        
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+
+        }
+
+        void OnEnable() {
+
+        }
+
+        void OnDisable() {
+
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+        void OnTriggerEnter2D(Collider2D other) {
+            LayerMask collisionLayerMask = 1 << other.gameObject.layer;
+            if ((collisionLayerMask & climbableLayerMask) != 0)
+            {
+                CanClimb = true;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other) {
+            LayerMask collisionLayerMask = 1 << other.gameObject.layer;
+            if ((collisionLayerMask & climbableLayerMask) != 0)
+            {
+                CanClimb = false;
+            }
+        }
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+    }
+}
