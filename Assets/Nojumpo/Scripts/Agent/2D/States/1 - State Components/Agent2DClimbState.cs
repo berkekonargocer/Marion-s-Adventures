f0ab13d0a8c5f84d448764927c73bbe414a27437
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-01 00:30:02 +0300

    Agent'dClimbState.cs Created

---
+++ Assets/Nojumpo/Scripts/Agent/2D/States/1 - State Components/Agent2DClimbState.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class Agent2DClimbState : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+        
+        
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+
+        }
+
+        void OnEnable() {
+
+        }
+
+        void OnDisable() {
+
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+        
+        
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+    }
+}
