95889dcd24606e3f243dd77aeb111d42ca7d0c97
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-01 03:53:34 +0300

    Change to Climb State from Fall and Jump States

--- Assets/Nojumpo/Scripts/Agent/2D/States/1 - State Components/Agent2DFallState.cs
+++ Assets/Nojumpo/Scripts/Agent/2D/States/1 - State Components/Agent2DFallState.cs
@@ -19,6 +19,12 @@ namespace Nojumpo
             CalculateVelocity();
             SetVelocity();
 
+            if (Mathf.Abs(inputReader.MovementVector.y) > 0 && _agent2D.ClimbableDetector.CanClimb)
+            {
+                _agent2D.ChangeState(climbState);
+                return;
+            }
+            
             if (_agent2D.GroundDetector.IsGrounded)
             {
                 _agent2D.ChangeState(idleState);
--- Assets/Nojumpo/Scripts/Agent/2D/States/1 - State Components/Agent2DJumpState.cs
+++ Assets/Nojumpo/Scripts/Agent/2D/States/1 - State Components/Agent2DJumpState.cs
@@ -52,6 +52,12 @@ namespace Nojumpo
             CalculateVelocity();
             SetVelocity();
 
+            if (Mathf.Abs(inputReader.MovementVector.y) > 0 && _agent2D.ClimbableDetector.CanClimb)
+            {
+                _agent2D.ChangeState(climbState);
+                return;
+            }
+            
             if (_agent2D.RigidBody2D.velocity.y <= 0)
             {
                 _agent2D.ChangeState(fallState);
