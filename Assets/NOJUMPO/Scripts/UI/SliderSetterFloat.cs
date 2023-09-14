20df0792d1ce395e8ba69351cee8b5440e88e1cf
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-14 20:59:45 +0300

    SliderSetterFloat.cs Created and Implemented

---
+++ Assets/NOJUMPO/Scripts/UI/SliderSetterFloat.cs
@@ -0,0 +1,40 @@
+using Nojumpo.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nojumpo.UI
+{
+    public class SliderSetterFloat : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+        [Tooltip("Slider to set")]
+        [SerializeField] Slider _slider;
+
+        [Tooltip("Float Variable Scriptable Object Value to equalize to the Slider value")]
+        [SerializeField] FloatVariableSO _variable;
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+            if (_slider == null)
+            {
+                Debug.Log($"Slider '{_slider}' is not set! disabling the component", gameObject);
+                enabled = false;
+                return;
+            }
+
+            if (_variable == null)
+            {
+                Debug.Log($"Variable '{_variable}' is not set! disabling the component", gameObject);
+                enabled = false;
+            }
+        }
+
+        void Update() {
+            if ((int)_slider.value == (int)_variable.Value)
+                return;
+
+            _slider.value = _variable.Value;
+        }
+    }
+}
