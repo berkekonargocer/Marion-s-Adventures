e905f3f28811b087353de4a076da77052acfdfca
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-22 03:34:25 +0300

    Weapon System 0.8

--- Assets/NOJUMPO/Systems/Weapon System/Scripts/Components/Class/WeaponStorage.cs
+++ Assets/NOJUMPO/Systems/Weapon System/Scripts/Components/Class/WeaponStorage.cs
@@ -1,36 +1,55 @@
-using System;
 using System.Collections.Generic;
 
 namespace Nojumpo.WeaponSystem
 {
-    /// <summary>
-    /// Add a list of weapons?
-    /// </summary>
     public class WeaponStorage
     {
         // -------------------------------- FIELDS --------------------------------
-        public int WeaponCount { get; private set; }
+        List<WeaponSO> _weaponList = new List<WeaponSO>();
+        int _currentWeaponIndex = -1;
+        bool _swapWeaponOnPickUp = true;
 
-        // ----------------------------- CONSTRUCTORS -----------------------------
-
-
-        // ------------------------ CUSTOM PRIVATE METHODS ------------------------
+        public int WeaponCount { get { return _weaponList.Count; } }
 
 
         // ------------------------ CUSTOM PUBLIC METHODS -------------------------
         public WeaponSO GetCurrentWeapon() {
-            throw new NotImplementedException();
+            return _currentWeaponIndex <= -1 ? null : _weaponList[_currentWeaponIndex];
         }
 
         public WeaponSO SwapWeapon() {
-            throw new NotImplementedException();
+            if (_currentWeaponIndex <= -1)
+                return null;
+
+            _currentWeaponIndex++;
+
+            if (_currentWeaponIndex >= _weaponList.Count)
+                _currentWeaponIndex = 0;
+
+            return _weaponList[_currentWeaponIndex];
         }
-        
+
         public void AddWeapon(WeaponSO weaponSO) {
-            throw new NotImplementedException();
+            if (_weaponList.Contains(weaponSO))
+                return;
+
+            _weaponList.Add(weaponSO);
+
+            if (_swapWeaponOnPickUp)
+            {
+                _currentWeaponIndex = _weaponList.Count - 1;
+            }
         }
+
         public List<string> GetPlayerWeaponNames() {
-            throw new NotImplementedException();
+            List<string> weaponNames = new List<string>();
+
+            for (int i = 0; i < _weaponList.Count - 1; i++)
+            {
+                weaponNames.Add(_weaponList[i].name);
+            }
+
+            return weaponNames;
         }
     }
 }
