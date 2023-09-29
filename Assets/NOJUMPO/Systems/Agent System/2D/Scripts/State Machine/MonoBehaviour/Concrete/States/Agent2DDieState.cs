ab9580afb65eaf2ce9a6e7860c7f8c6ea3abd5f1
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-29 07:43:56 +0300

    Agent2DGetHit and Agent2DDie States

---
+++ Assets/NOJUMPO/Systems/Agent System/2D/Scripts/State Machine/MonoBehaviour/Concrete/States/Agent2DDieState.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class Agent2DDieState : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+        
+        
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+
+        }
+
+        void OnEnable() {
+
+        }
+
+        void OnDisable() {
+
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+
+	// ------------------------- CUSTOM PUBLIC METHODS -------------------------
+	
+	
+	
+	// ------------------------ CUSTOM PROTECTED METHODS -----------------------
+	
+	
+	
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+        
+    }
+}
