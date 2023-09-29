212144faee5714c857241ce52eea0a53f629d54b
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-29 14:33:45 +0300

    Ground Detector Gizmo Fix | Mini Tyrannasourus 0.2 Ground Detector Settings

--- Assets/NOJUMPO/Systems/Agent System/2D/Scripts/Components/MonoBehaviour/Agent2DGroundDetector.cs
+++ Assets/NOJUMPO/Systems/Agent System/2D/Scripts/Components/MonoBehaviour/Agent2DGroundDetector.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Nojumpo
 {
@@ -6,14 +7,14 @@ namespace Nojumpo
     public class Agent2DGroundDetector : MonoBehaviour
     {
         // -------------------------------- FIELDS ---------------------------------
+        [SerializeField] Collider2D agent2DCollider;
+        
         [SerializeField] LayerMask groundLayerMask;
         [Range(-2f, 2f)] [SerializeField] float boxCastYOffset = -0.4f;
         [Range(-2f, 2f)] [SerializeField] float boxCastXOffset;
         [Range(0f, 2f)] [SerializeField] float boxCastWidth = 1.0f, boxCastHeight = 0.5f;
 
         [SerializeField] Color groundedColor = Color.green, notGroundedGizmoColor = Color.red;
-
-        Collider2D _agent2DCollider;
 
         RaycastHit2D[] _groundedHits = new RaycastHit2D[1];
 
@@ -26,7 +27,7 @@ namespace Nojumpo
         }
 
         void OnDrawGizmos() {
-            if (_agent2DCollider == null)
+            if (agent2DCollider == null)
                 return;
 
             Gizmos.color = notGroundedGizmoColor;
@@ -34,19 +35,19 @@ namespace Nojumpo
             if (IsGrounded)
                 Gizmos.color = groundedColor;
 
-            Gizmos.DrawWireCube(_agent2DCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset, 0),
+            Gizmos.DrawWireCube(agent2DCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset, 0),
                 new Vector3(boxCastWidth, boxCastHeight));
         }
 
-        
+
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
         public void CheckIsGrounded() {
-            int groundedHits = Physics2D.BoxCastNonAlloc(_agent2DCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset, 0),
+            int groundedHits = Physics2D.BoxCastNonAlloc(agent2DCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset, 0),
                 new Vector2(boxCastWidth, boxCastHeight), 0, Vector2.down, _groundedHits, 0, groundLayerMask);
 
             if (groundedHits == 1)
             {
-                if (_groundedHits[0].collider.IsTouching(_agent2DCollider))
+                if (_groundedHits[0].collider.IsTouching(agent2DCollider))
                     IsGrounded = true;
             }
             else
@@ -54,11 +55,11 @@ namespace Nojumpo
                 IsGrounded = false;
             }
         }
-        
-        
+
+
         // ------------------------- CUSTOM PRIVATE METHODS ------------------------
         void SetComponents() {
-            _agent2DCollider = GetComponent<Collider2D>();
+            agent2DCollider = GetComponent<Collider2D>();
         }
     }
 }
