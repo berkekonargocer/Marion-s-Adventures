524fd9d99c6447b1335ea4529dec94de9b8ccc87
Berke Konargöçer <berke.konargocer@gmail.com>
2023-10-01 19:29:42 +0300

    Agent2D AI2D System 0.1

---
+++ Assets/NOJUMPO/Systems/Agent System/2D/AI/Scripts/State Machines/MonoBehaviour/Abstract/AI2DState.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public abstract class AI2DState : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
---
+++ Assets/NOJUMPO/Systems/Agent System/2D/AI/Scripts/State Machines/MonoBehaviour/Concrete/States/AI2DWalkState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class AI2DWalkState : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+        }
+
+        void OnEnable() {
+        }
+
+        void OnDisable() {
+        }
+
+        void Start() {
+        }
+
+        void Update() {
+        }
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
