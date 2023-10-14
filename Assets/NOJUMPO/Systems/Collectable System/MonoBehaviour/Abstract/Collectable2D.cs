d0fee9a38a0e950ef95b56ad07e6f4ecb4b3d805
Berke Konargöçer <berke.konargocer@gmail.com>
2023-10-15 00:08:03 +0300

    Player Prefab Changes Applied

--- Assets/NOJUMPO/Systems/Collectable System/MonoBehaviour/Abstract/Collectable2D.cs
+++ Assets/NOJUMPO/Systems/Collectable System/MonoBehaviour/Abstract/Collectable2D.cs
@@ -12,7 +12,6 @@ namespace Nojumpo.CollectableSystem
         [SerializeField] AudioSource sfxAudioSource;
         
         
-
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
         void OnEnable() {
             if (sfxAudioSource == null)
